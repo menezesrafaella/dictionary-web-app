56a1a8aceb7d365edbbc214243874d3a88d6a30c
Rafaella <carmo.rafaella@yahoo.com.br>
2023-04-11 19:30:50 -0300

    feat: init project react, create component navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Logo from '../assets/images/logo.svg'
+
+function Navbar() {
+  return (
+    <header>
+        <img src={Logo} alt=''/>
+        {/* //select
+        //toggle
+        //lua */}
+
+    </header>
+  )
+}
+
+export default Navbar
